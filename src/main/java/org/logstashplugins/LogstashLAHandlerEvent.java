36c78b8368d7302de58d4911bad2485c53e70a4e
RoeiDimi <roei.dimi@gmail.com>
2025-03-06 12:54:28 +0000

    cr comments

--- src/main/java/org/logstashplugins/LogstashLAHandlerEvent.java
+++ src/main/java/org/logstashplugins/LogstashLAHandlerEvent.java
@@ -9,6 +9,10 @@ import org.logstashplugins.LogAnalyticsEventsHandler.LAEventsHandlerEvent;
 public class LogstashLAHandlerEvent extends HashMap<String, Object> implements LAEventsHandlerEvent {
     public LogstashLAHandlerEvent(Map<String, Object> eventDataAsIdentityMap, List<String> keysToKeep) {
         HashMap<String, Object> eventDataMap = new HashMap<String, Object>(eventDataAsIdentityMap);
+        if (keysToKeep.isEmpty()) {
+            this.putAll(eventDataMap);
+            return;
+        }
         for (String key : keysToKeep) {
             this.put(key, eventDataMap.get(key));
         }        
