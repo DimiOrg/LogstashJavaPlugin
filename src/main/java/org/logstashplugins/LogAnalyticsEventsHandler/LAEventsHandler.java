98a739dffaab79a70822c884e1779ad193dffd1c
RoeiDimi <roei.dimi@gmail.com>
2025-03-18 14:31:58 +0000

    Low traffic management + Workers unit tests

--- src/main/java/org/logstashplugins/LogAnalyticsEventsHandler/LAEventsHandler.java
+++ src/main/java/org/logstashplugins/LogAnalyticsEventsHandler/LAEventsHandler.java
@@ -11,50 +11,65 @@ import java.util.concurrent.*;
 
 import org.logstashplugins.LogAnalyticsEventsHandler.Workers.BatcherWorker;
 import org.logstashplugins.LogAnalyticsEventsHandler.Workers.SenderWorker;
+import org.logstashplugins.LogAnalyticsEventsHandler.Workers.UnifierWorker;
 import org.logstashplugins.LogAnalyticsEventsHandler.Workers.Worker;
 
 public class LAEventsHandler {
     private static final Logger logger = LoggerFactory.getLogger(LAEventsHandler.class);
 
     private LAEventsHandlerConfiguration configuration;
-    private BlockingQueue<LAEventsHandlerEvent> eventsQueue;
-    private BlockingQueue<List<Object>> batchesQueue;
+
+    private BlockingQueue<LAEventsHandlerEvent> inputQueue;
+    private BlockingQueue<List<Object>> sendersQueue;
+    private BlockingQueue<List<Object>> unifiersQueue;
+
     private ScheduledExecutorService batchersExecutorService;
     private ScheduledExecutorService sendersExecutorService;
+    private ScheduledExecutorService unifiersExecutorService;
     private Set<BatcherWorker> batcherWorkers;
     private Set<SenderWorker> senderWorkers;
+    private Set<UnifierWorker> unifierWorkers;
 
     public LAEventsHandler(LAEventsHandlerConfiguration configuration) {
-        eventsQueue = new LinkedBlockingQueue<>();
-        batchesQueue = new LinkedBlockingQueue<>();
+        inputQueue = new LinkedBlockingQueue<>();
+        unifiersQueue = new LinkedBlockingQueue<>();
+        sendersQueue = new LinkedBlockingQueue<>();
+
         this.configuration = configuration;
         int batcherWorkerCount = getBatcherWorkerCount();
         int senderWorkerCount = getSenderWorkerCount();
+        int unifierWorkerCount = getUnifierWorkerCount();
         batchersExecutorService = Executors.newScheduledThreadPool(batcherWorkerCount); 
         sendersExecutorService = Executors.newScheduledThreadPool(batcherWorkerCount); 
+        unifiersExecutorService = Executors.newScheduledThreadPool(unifierWorkerCount);
         batcherWorkers = new HashSet<>();
         senderWorkers = new HashSet<>();
-
-        logger.info("Starting Log Analytics Events Handler with {} batcher workers and {} sender workers", batcherWorkerCount, senderWorkerCount);
+        unifierWorkers = new HashSet<>();
+        logger.info("Starting Log Analytics Events Handler with {} batcher workers and {} sender workers and {} unifier workers", batcherWorkerCount, senderWorkerCount, unifierWorkerCount);
         
         // Start batcher workers and sender workers. The ScheduledExecutorService will make sure that terminated workers are restarted.
         for (int i = 0; i < batcherWorkerCount; i++) {
-            BatcherWorker batcherWorker = new BatcherWorker(eventsQueue, batchesQueue, configuration.getBatcherWorkerConfig());
+            BatcherWorker batcherWorker = new BatcherWorker(inputQueue, sendersQueue, unifiersQueue, configuration.getBatcherWorkerConfig());
             batchersExecutorService.scheduleAtFixedRate(batcherWorker, 0, 1, TimeUnit.MINUTES);
             batcherWorkers.add(batcherWorker);
         }
         for (int i = 0; i < senderWorkerCount; i++) {
-            SenderWorker senderWorker = new SenderWorker(batchesQueue, configuration.getSenderWorkerConfig());
+            SenderWorker senderWorker = new SenderWorker(sendersQueue, configuration.getSenderWorkerConfig());
             sendersExecutorService.scheduleAtFixedRate(senderWorker, i, 1, TimeUnit.MINUTES);
             senderWorkers.add(senderWorker);
+        }
+        for (int i = 0; i < unifierWorkerCount; i++) {
+            UnifierWorker unifierWorker = new UnifierWorker(unifiersQueue, sendersQueue, configuration.getUnifierWorkerConfig());
+            unifiersExecutorService.scheduleAtFixedRate(unifierWorker, 0, 1, TimeUnit.MINUTES);
+            unifierWorkers.add(unifierWorker);
         }
 
         logger.info("Log Analytics Events Handler has been started");
     }
 
     public void handle(LAEventsHandlerEvent event) {
-        logger.debug("Events queue size: {}", eventsQueue.size());
-        eventsQueue.add(event);
+        logger.debug("Events queue size: {}", inputQueue.size());
+        inputQueue.add(event);
     }
 
     public void shutdown() {
@@ -62,23 +77,33 @@ public class LAEventsHandler {
         int shutdownTimeSeconds = Optional.ofNullable(configuration.getLaEventsHandlerConfig().getMaxGracefulShutdownTimeSeconds())
                            .filter(time -> time != 0)
                            .orElse(60);
-        double batchersToSendersRatio = (double) batcherWorkers.size() / senderWorkers.size();
-        int batchersMaxTimeToShutdown = (int) (shutdownTimeSeconds * batchersToSendersRatio);
 
-        // call shutdown on workers to stop them gracefully. First the batchers, then the senders
+        int totalWorkerCount = batcherWorkers.size() + senderWorkers.size() + unifierWorkers.size();
+        int batchersMaxTimeToShutdown = shutdownTimeSeconds * (batcherWorkers.size() / totalWorkerCount);
+        int unifiersMaxTimeToShutdown = shutdownTimeSeconds * (unifierWorkers.size() / totalWorkerCount);
+        int sendersMaxTimeToShutdown = shutdownTimeSeconds * (senderWorkers.size() / totalWorkerCount);
+
+        // call shutdown on workers to stop them gracefully. First the batchers, then the unifiers and finally the senders
         shutdownWorkersConcurrently(batcherWorkers, batchersMaxTimeToShutdown);
         logger.info("Batchers have been shut down");
-        shutdownWorkersConcurrently(senderWorkers, shutdownTimeSeconds - batchersMaxTimeToShutdown);
+        shutdownWorkersConcurrently(unifierWorkers, unifiersMaxTimeToShutdown);
+        logger.info("Unifiers have been shut down");
+        shutdownWorkersConcurrently(senderWorkers, sendersMaxTimeToShutdown);
         logger.info("Senders have been shut down");
 
         // shutdown the executor services
         batchersExecutorService.shutdown();
+        unifiersExecutorService.shutdown();
         sendersExecutorService.shutdown();
 
         try {
             if (!batchersExecutorService.awaitTermination(shutdownTimeSeconds , TimeUnit.SECONDS)) {
                 logger.warn("Batchers executor service did not terminate in the specified time. Forcing shutdown.");
                 batchersExecutorService.shutdownNow();
+            }
+            if (!unifiersExecutorService.awaitTermination(shutdownTimeSeconds , TimeUnit.SECONDS)) {
+                logger.warn("Unifiers executor service did not terminate in the specified time. Forcing shutdown.");
+                unifiersExecutorService.shutdownNow();
             }
             if (!sendersExecutorService.awaitTermination(shutdownTimeSeconds , TimeUnit.SECONDS)) {
                 logger.warn("Senders executor service did not terminate in the specified time. Forcing shutdown.");
@@ -88,6 +113,7 @@ public class LAEventsHandler {
             logger.warn("Interrupted while waiting for executor services to terminate. Forcing shutdown.");
             batchersExecutorService.shutdownNow();
             sendersExecutorService.shutdownNow();
+            unifiersExecutorService.shutdownNow();
             Thread.currentThread().interrupt();
         }
         logger.info("Log Analytics Events Handler has been shut down");
@@ -101,9 +127,15 @@ public class LAEventsHandler {
                 logger.warn("Sender worker is still running");
             }
         }
+        for (UnifierWorker unifierWorker : unifierWorkers) {
+            if (unifierWorker.isRunning()) {
+                logger.warn("Unifier worker is still running");
+            }
+        }
 
-        logger.debug("Events queue size: {}", eventsQueue.size());
-        logger.debug("Batches queue size: {}", batchesQueue.size());
+        logger.debug("Input queue size: {}", inputQueue.size());
+        logger.debug("Unifiers queue size: {}", unifiersQueue.size());
+        logger.debug("Senders queue size: {}", sendersQueue.size());
     }
 
     private void shutdownWorkersConcurrently(Set<? extends Worker> workers, int maxGracefulShutdownTimeSeconds) {
@@ -122,11 +154,16 @@ public class LAEventsHandler {
 
     private int getBatcherWorkerCount() {
         int availableProcessors = Runtime.getRuntime().availableProcessors();
-        return (availableProcessors - 1) / 2;
+        return (availableProcessors - 1) / 3;
     }
 
     private int getSenderWorkerCount() {
         int availableProcessors = Runtime.getRuntime().availableProcessors();
-        return (availableProcessors - 1) / 2;
+        return (availableProcessors - 1) / 3;
+    }
+
+    private int getUnifierWorkerCount() {
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        return (availableProcessors - 1) / 3;
     }
 }
--- src/main/java/org/logstashplugins/MicrosoftSentinelOutput.java
+++ src/main/java/org/logstashplugins/MicrosoftSentinelOutput.java
@@ -52,6 +52,8 @@ public class MicrosoftSentinelOutput implements Output {
 
     public static final PluginConfigSpec<Long> MAX_GRACEFUL_SHUTDOWN_TIME_SECONDS_CONFIG =
             PluginConfigSpec.numSetting("max_graceful_shutdown_time_seconds", 60);
+    public static final PluginConfigSpec<Long> MAX_WAITING_FOR_UNIFIER_TIME_SECONDS_CONFG = 
+            PluginConfigSpec.numSetting("max_waiting_for_unifier_time_seconds", 10);
 
     private final String id;
     private final CountDownLatch done = new CountDownLatch(1);
@@ -99,7 +101,7 @@ public class MicrosoftSentinelOutput implements Output {
         eventsHandlerConfiguration.getSenderWorkerConfig().setTenantId(config.get(TENANT_ID_CONFIG));
 
         eventsHandlerConfiguration.getBatcherWorkerConfig().setMaxWaitingTimeSecondsForBatch(config.get(MAX_WAITING_TIME_FOR_BATCH_SECONDS_CONFIG).intValue());
-
+        eventsHandlerConfiguration.getUnifierWorkerConfig().setMaxWaitingForUnifierTimeSeconds(config.get(MAX_WAITING_FOR_UNIFIER_TIME_SECONDS_CONFG).intValue());
         eventsHandlerConfiguration.getLaEventsHandlerConfig().setMaxGracefulShutdownTimeSeconds(config.get(MAX_GRACEFUL_SHUTDOWN_TIME_SECONDS_CONFIG).intValue());
 
         return eventsHandlerConfiguration;
@@ -132,7 +134,8 @@ public class MicrosoftSentinelOutput implements Output {
             CLIENT_SECRET_CONFIG,
             TENANT_ID_CONFIG,
             MAX_WAITING_TIME_FOR_BATCH_SECONDS_CONFIG,
-            MAX_GRACEFUL_SHUTDOWN_TIME_SECONDS_CONFIG
+            MAX_GRACEFUL_SHUTDOWN_TIME_SECONDS_CONFIG,
+            MAX_WAITING_FOR_UNIFIER_TIME_SECONDS_CONFG
         );
     }
 
