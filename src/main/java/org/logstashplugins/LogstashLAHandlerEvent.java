4d6ec903c7533104b8fa1d4590430f7c86fb3658
RoeiDimi <roei.dimi@gmail.com>
2025-03-25 09:34:21 +0000

    Scale Testing - improvements and cleanups

--- src/main/java/org/logstashplugins/LogstashLAHandlerEvent.java
+++ src/main/java/org/logstashplugins/LogstashLAHandlerEvent.java
@@ -9,7 +9,7 @@ import org.logstashplugins.LogAnalyticsEventsHandler.LAEventsHandlerEvent;
 public class LogstashLAHandlerEvent extends HashMap<String, Object> implements LAEventsHandlerEvent {
     public LogstashLAHandlerEvent(Map<String, Object> eventDataAsIdentityMap, List<String> keysToKeep) {
         HashMap<String, Object> eventDataMap = new HashMap<String, Object>(eventDataAsIdentityMap);
-        if (keysToKeep.isEmpty()) {
+        if (keysToKeep == null || keysToKeep.isEmpty()) {
             this.putAll(eventDataMap);
             return;
         }
